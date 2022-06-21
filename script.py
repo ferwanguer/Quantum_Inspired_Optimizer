86bd6e7a8426273a2124fb74c30520ddd334d958
fwanguem <fernando.wanguemert-guerra@altran.com>
2022-06-21 13:21:17 +0200

    File from which to run the algorithm. All parameters are tunable from this file

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ import numpy as np
 from qea import QuantumEvAlgorithm
 import time
 import os
-from main import f,g,rastrigin,rosenbrock
+from test_functions import f,g,rastrigin,rosenbrock
 
-optimizer = QuantumEvAlgorithm(f, n_dims = 50,sigma_scaler = 1.0005,
+optimizer = QuantumEvAlgorithm(f, n_dims = 500,sigma_scaler = 1.0005,
                                    mu_scaler = 50, elitist_level = 2)
-optimizer.training(N_iterations=200000, sample_size= 10)
+
+optimizer.training(N_iterations=150000, sample_size= 10, sample_increaser_factor=0,save_results= False ,filename= 'testing_evl.npz')
