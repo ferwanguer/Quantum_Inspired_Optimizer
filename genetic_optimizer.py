165684b9f19c0b3cf8d0770735489564fe771222
Personal laptop <f.wguerra@outlook.com>
2022-06-26 14:03:02 +0200

    Set up finished

--- genetic_optimizer.py
+++ genetic_optimizer.py
@@ -3,7 +3,7 @@ from test_functions import f, g, rastrigin, rosenbrock
 from geneticalgorithm import geneticalgorithm as ga
 
 varbound=np.array([[-5.12,5.12]]*30)
-
+# Ojo con con el cambio que he hecho en la librería. Reapsar mañana.
 
 model=ga(function=rastrigin,dimension=30,variable_type='real',variable_boundaries=varbound)
 
