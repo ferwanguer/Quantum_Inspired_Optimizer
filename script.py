9002209bb20d86d8698eaeaeccb795d3fa09731f
Personal laptop <f.wguerra@outlook.com>
2022-06-26 14:02:04 +0200

    Testing different algorithms with progress bar. added Ros_flag... further work to be done here.

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 from qea import QuantumEvAlgorithm
 from test_functions import f,g,rastrigin,rosenbrock
 
-optimizer = QuantumEvAlgorithm(f, n_dims = 5000,sigma_scaler = 1.00005,
-                                   mu_scaler = 100, elitist_level = 2)
+optimizer = QuantumEvAlgorithm(rosenbrock, n_dims = 20,sigma_scaler = 1.0001,
+                                   mu_scaler = 20, elitist_level = 2, ros_flag=False)
 
-optimizer.training(N_iterations=300000, sample_size= 10, sample_increaser_factor=0,save_results= True ,filename= 'testing_evl.npz')
+optimizer.training(N_iterations=500000, sample_size= 10, sample_increaser_factor=0,save_results= True ,filename= 'testing_evl.npz')
