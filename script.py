5d64c9694dda595c6a50b5b585adf22a5de986ab
fwanguem <ferwanguer@me.com>
2022-08-05 12:42:23 +0200

    running griewank optimization

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ from qea import QuantumEvAlgorithm
 from test_functions import f,g,rastrigin,rosenbrock, griewank
 
 
-optimizer = QuantumEvAlgorithm(griewank, n_dims = 200,sigma_scaler = 1.0002,
+optimizer = QuantumEvAlgorithm(griewank, n_dims = 100,sigma_scaler = 1.001,
                                    mu_scaler = 50, elitist_level = 4, ros_flag=False)
 
-optimizer.training(N_iterations=100000, sample_size= 10, save_results= True,filename='q0.npz')
+optimizer.training(N_iterations=40000, sample_size= 20, save_results= True,filename='q0.npz')
 
