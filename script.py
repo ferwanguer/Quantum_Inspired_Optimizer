b3bd9ac72ddcde0f60f7d9e3debab5d11f19a61f
fwanguem <ferwanguer@me.com>
2022-08-22 16:37:50 +0200

    errata

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from qea import QuantumEvAlgorithm
 from test_functions import f,g,rastrigin,rosenbrock, griewank
 
 
-optimizer = QuantumEvAlgorithm(rastrigin, n_dims = 10000,sigma_scaler = 1.0000001,
+optimizer = QuantumEvAlgorithm(rastrigin, n_dims = 100,sigma_scaler = 1.0001,
                                    mu_scaler = 100, elitist_level = 4, ros_flag=False, saving_interval=20)
 
 optimizer.training(N_iterations=4_000_000, sample_size= 10, save_results= True,filename='q10.npz')
