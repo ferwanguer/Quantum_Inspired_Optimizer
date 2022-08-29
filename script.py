5346572695d820abef82ec8d599e58fbef37287a
fwanguem <ferwanguer@me.com>
2022-08-29 09:10:15 +0200

    added new cost functions

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 
 from qea import QuantumEvAlgorithm
-from test_functions import f,g,rastrigin,rosenbrock, griewank
+from test_functions import f,g,rastrigin,rosenbrock, griewank, michael, schwefel
 
 
-optimizer = QuantumEvAlgorithm(f, n_dims = 100,sigma_scaler = 1.005,
-                                   mu_scaler = 10, elitist_level = 4, ros_flag= False, saving_interval=20)
+optimizer = QuantumEvAlgorithm(michael, n_dims = 2,sigma_scaler = 1.005,
+                                   mu_scaler = 5, elitist_level = 2, ros_flag= False, saving_interval=20)
 
-optimizer.training(N_iterations=100_000, sample_size= 10, save_results= True,filename='q11.npz')
+optimizer.training(N_iterations=1000, sample_size= 5, save_results= True,filename='q11.npz')
 
