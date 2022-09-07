f8df4ab02c71910713cdeb7743d050950f7a316a
fwanguem <ferwanguer@me.com>
2022-09-07 12:45:09 +0200

    stashh

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from qea import QuantumEvAlgorithm
 from test_functions import f,g,rastrigin,rosenbrock, griewank, michael, schwefel
 
 
-optimizer = QuantumEvAlgorithm(g, n_dims = 4500,sigma_scaler = 1.001,
+optimizer = QuantumEvAlgorithm(g, n_dims = 4500,sigma_scaler = 1.0001,
                                    mu_scaler = 5, elitist_level = 3, ros_flag= False, saving_interval=100)
 
 optimizer.training(N_iterations=100000, sample_size= 10, save_results= True,filename='q11.npz')
